f2dcf6b9e2bdb50f136575d22312c534ca2b5750
Amanda <amandajuhlsilberg@hotmail.com>
2025-05-05 16:59:15 +0200

    Started the basic MVVM structure of the program. Made SuperClass "Being" and two sub-classes "Player" and "Enemy"

---
+++ RpgFight/RpgFight/Model/Classes/Being.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgFight.Model.Classes
+{
+    internal class Being
+    {
+      public string Name { get; set; }
+      public int HealthPoints { get; set; }
+      public bool IsAlive { get; set; }
+      public string AttackType { get; set; }
+      public int AttackPower { get; set; }
+
+
+    public Being() { }
+
+    
+
+    }
+}
