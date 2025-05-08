760fd184933b132d590c4184184a5e0b1923dbd0
Amanda <amandajuhlsilberg@hotmail.com>
2025-05-08 10:28:52 +0200

    Started new project to make multiple views

---
+++ MultipleViews/MultipleViews/MVVM/ViewModel/BoardgamesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleViews.MVVM.ViewModel
+{
+    internal class BoardgamesViewModel
+    {
+    }
+}
