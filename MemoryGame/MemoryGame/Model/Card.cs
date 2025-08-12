3473b53fd28edc318f2e6507041b85c120d91e9e
Amanda <amandajuhlsilberg@hotmail.com>
2025-08-12 10:18:05 +0200

    Made folders and two classes

---
+++ MemoryGame/MemoryGame/Model/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame.Model
+{
+    internal class Card
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+        public bool IsFlipped { get; set; }
+        public bool IsMatched { get; set; }
+
+
+        public Card(int id, string symbol, bool isFlipped, bool isMatched) 
+        {
+            this.Id = id;
+            this.Symbol = symbol;
+            this.IsFlipped = isFlipped;
+            this.IsMatched = isMatched;
+        }
+        
+
+    }
+}
---
+++ MemoryGame/MemoryGame/Model/GameStats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame.Model
+{
+    internal class GameStats
+    {
+        public string PlayerName { get; set; }
+
+        public int Moves { get; set; }
+
+        public TimeSpan GameTime { get; set; }
+
+        public DateTime CompletedAt { get; set; }
+
+        public GameStats(string playerName, int moves, TimeSpan gameTime, DateTime completedAt) 
+        { 
+            this.PlayerName = playerName;
+            this.Moves = moves;
+            this.GameTime = gameTime;
+            this.CompletedAt = completedAt;
+
+        }
+    }
+}
