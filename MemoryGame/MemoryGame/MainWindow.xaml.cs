d049e9de342a4c5ef5b78bea6d42ca948440fb2a
Amanda <amandajuhlsilberg@hotmail.com>
2025-08-13 14:57:47 +0200

    Who know's what's going on at this point...

--- MemoryGame/MemoryGame/MainWindow.xaml.cs
+++ MemoryGame/MemoryGame/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MemoryGame.ViewModel;
 
 namespace MemoryGame
 {
@@ -19,6 +20,8 @@ namespace MemoryGame
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
         }
     }
 }
---
+++ MemoryGame/MemoryGame/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemoryGame.Model;
+
+
+namespace MemoryGame.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        // Adding a search function for the highscore:  // Following a turtorial on how to add a textbox:
+        private ICollectionView _hSView;
+        public ICollectionView HSView => _hSView;
+        private string _textToFilter;
+
+        public string TextToFilter
+        {
+            get { return _textToFilter; }
+            set
+            {
+                _textToFilter = value;
+                OnPropertyChanged();
+                _hSView?.Refresh(); //This triggers the filtering logic
+            }
+        }
+        // ends here
+        //public ObservableCollection<Card> Cards {  get; set; } = new ObservableCollection<Card>(); // To make a collection of cards. <- Maybe
+
+        /*
+         Properties:
+public ObservableCollection<Card> Cards { get; set; }
+public string PlayerName { get; set; }
+public int MoveCount { get; set; }
+public string GameTime { get; set; }
+public bool IsGameCompleted { get; set; }
+public Card FirstSelectedCard { get; set; }
+public Card SecondSelectedCard { get; set; }
+Commands:
+•	FlipCardCommand
+•	NewGameCommand
+•	SaveStatsCommand
+
+        */
+
+
+    }
+
+
+}
+}
