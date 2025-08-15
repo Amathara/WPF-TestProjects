95d2cf5955ad48e5e71a91bb307924b0e72e17ff
Amanda <amandajuhlsilberg@hotmail.com>
2025-08-15 16:10:31 +0200

    Made an empty msTest

---
+++ MemoryGame/Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿using MemoryGame;
+namespace Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
